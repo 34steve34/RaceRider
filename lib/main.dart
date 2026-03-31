787c82ba242b7d15ac1307df24de2b31cc6afdc0
34steve34 <stevesaad@gmail.com>
2026-03-31 08:10:52 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
-import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(GameWidget(game: RaceRiderGame()));
 }
 
-class RaceRiderGame extends FlameGame {
+class RaceRiderGame extends Forge2DGame {
+  // Setting a strong, smooth gravity for that "exquisite" feel
+  RaceRiderGame() : super(gravity: Vector2(0, 15.0));
+
   @override
   Future<void> onLoad() async {
-    print("RaceRider Engine Online");
+    await super.onLoad();
+    debugPrint("RaceRider Physics Engine: ONLINE");
   }
 }
