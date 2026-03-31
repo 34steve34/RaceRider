4155198e383376e6dd38b522bc232aa514be9c9d
34steve34 <stevesaad@gmail.com>
2026-03-31 08:24:08 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flame/game.dart';
-import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:flame_forge2d/flame_forge2d.dart'; // This includes the correct Vector2
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,7 +7,7 @@ void main() {
 }
 
 class RaceRiderGame extends Forge2DGame {
-  // Setting a strong, smooth gravity for that "exquisite" feel
+  // Use Vector2 directly from the Forge2D library
   RaceRiderGame() : super(gravity: Vector2(0, 15.0));
 
   @override
