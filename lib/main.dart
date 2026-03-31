db0158974344cf1b475e3f50f9ab158fdab81010
34steve34 <stevesaad@gmail.com>
2026-03-31 14:19:18 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,27 @@
 import 'package:flame/game.dart';
-import 'package:flame_forge2d/flame_forge2d.dart'; // This includes the correct Vector2
+import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/material.dart';
+import 'track.dart'; // Import our new track file
 
 void main() {
   runApp(GameWidget(game: RaceRiderGame()));
 }
 
 class RaceRiderGame extends Forge2DGame {
-  // Use Vector2 directly from the Forge2D library
+  // Strong gravity for the "heavy" feel
   RaceRiderGame() : super(gravity: Vector2(0, 15.0));
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
-    debugPrint("RaceRider Physics Engine: ONLINE");
+    
+    // Set up the camera to look at our track
+    camera.viewfinder.position = Vector2(20, -5);
+    camera.viewfinder.zoom = 15.0; // Zoom in (Forge2D uses meters, not pixels)
+
+    // Drop the track into the world!
+    world.add(TrackComponent());
+    
+    debugPrint("RaceRider: Track Loaded");
   }
 }
