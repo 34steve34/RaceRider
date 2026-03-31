5e81cd3114c38c91a8fb4a431464d606e7cbd3b6
34steve34 <stevesaad@gmail.com>
2026-03-31 14:47:36 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/material.dart';
 
 import 'track.dart';
-import 'bike.dart'; // Import the crash test dummy!
+import 'bike.dart'; // Import our new 3-piece assembly
 
 void main() {
   runApp(const GameWidget.controlled(gameFactory: RaceRiderGame.new));
@@ -16,17 +16,14 @@ class RaceRiderGame extends Forge2DGame {
   Future<void> onLoad() async {
     await super.onLoad();
     
-    // Position the camera to see the drop
     camera.viewfinder.position = Vector2(20, -5);
     camera.viewfinder.zoom = 15.0;
 
-    // 1. Build the ground
     world.add(TrackComponent());
     
-    // 2. Drop the chassis from the sky!
-    // X = 5 (above the first hill), Y = -20 (20 meters in the air)
-    world.add(BikeChassis(initialPosition: Vector2(5, -20)));
+    // Drop the fully assembled Bike!
+    world.add(Bike(initialPosition: Vector2(5, -20)));
     
-    debugPrint("RaceRider: Crash Test Dummy Deployed");
+    debugPrint("RaceRider: Multi-body Bike Deployed");
   }
 }
