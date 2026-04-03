dbf300bfd906fd9a7ef063154cc9ae6e0f501334
34steve34 <stevesaad@gmail.com>
2026-04-03 09:51:55 -0400

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'track.dart';
 import 'bike.dart';
 
 // --- BUMP VERSION TO TRACK REFRESHES ---
-const String gameVersion = "v1.3.1"; 
+const String gameVersion = "v1.3.2"; 
 
 void main() {
   runApp(GameWidget(game: RaceRiderGame()));
