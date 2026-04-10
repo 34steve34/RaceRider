cc74eeb96ccf34923803fd2177301f31ce7280bf
34steve34 <stevesaad@gmail.com>
2026-04-09 21:54:13 -0400

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Column; // Prevent name clashes
 import 'package:flame/game.dart';
 import 'package:flame/events.dart';
-// We use 'hide' to ensure Vector2 only comes from one place
-import 'package:flame_forge2d/flame_forge2d.dart' hide Vector2; 
-import 'package:forge2d/forge2d.dart' hide Vector2;
-import 'package:vector_math/vector_math.dart'; // This is the 32-bit one we want
+import 'package:flame_forge2d/flame_forge2d.dart' hide Vector2, World; 
 import 'package:sensors_plus/sensors_plus.dart';
-
+// FORCE everything to use the 64-bit version that Forge2D 0.17.1 wants
+import 'package:vector_math/vector_math_64.dart'; 
 
 void main() {
-  // We use the GameWidget from flame/game.dart
   runApp(
     MaterialApp(
       home: Scaffold(
@@ -22,7 +19,7 @@ void main() {
 }
 
 class ResetRacingGame extends Forge2DGame with TapCallbacks {
-  // Zoom 20 ensures 1 physics meter = 20 pixels. No more tiny dots.
+  // Use Vector2 from vector_math_64
   ResetRacingGame() : super(gravity: Vector2(0, 15), zoom: 20);
 
   late PlayerBox player;
@@ -30,14 +27,10 @@ class ResetRacingGame extends Forge2DGame with TapCallbacks {
 
   @override
   Future<void> onLoad() async {
-    // Add ground at the bottom of the screen
     add(GroundLine());
-    
-    // Start player in the middle-top
     player = PlayerBox(Vector2(0, -5));
     add(player);
 
-    // Listen for tilt
     accelerometerEvents.listen((AccelerometerEvent event) {
       tiltX = event.x; 
     });
@@ -46,17 +39,14 @@ class ResetRacingGame extends Forge2DGame with TapCallbacks {
   @override
   void update(double dt) {
     super.update(dt);
-    // Apply tilt force
     player.body.applyForce(Vector2(-tiltX * 40, 0));
   }
 
   @override
   void onTapDown(TapDownEvent event) {
     if (event.localPosition.x > size.x / 2) {
-      // Right Side: Gas/Jump
       player.body.applyLinearImpulse(Vector2(5, -15));
     } else {
-      // Left Side: Brake
       player.body.linearVelocity.x *= 0.2;
     }
   }
@@ -76,10 +66,10 @@ class PlayerBox extends BodyComponent {
 
   @override
   void render(Canvas canvas) {
-    // A visible 2x2 meter blue square
+    // Explicitly using the Flutter color to avoid the vector_math 'Colors' conflict
     canvas.drawRect(
       Rect.fromLTWH(-1, -1, 2, 2), 
-      Paint()..color = Colors.blue
+      Paint()..color = const Color(0xFF2196F3) // Blue
     );
   }
 }
@@ -87,7 +77,6 @@ class PlayerBox extends BodyComponent {
 class GroundLine extends BodyComponent {
   @override
   Body createBody() {
-    // A long line 5 meters down from center
     final shape = EdgeShape()..set(Vector2(-100, 5), Vector2(100, 5));
     return world.createBody(BodyDef(type: BodyType.static))
       ..createFixture(FixtureDef(shape));
