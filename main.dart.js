63bdf427f6b30865313348ea2795cf3978862e68
34steve34 <34steve34@users.noreply.github.com>
2026-05-03 19:58:10 +0000

    deploy: 6bc889e4ae5a637368d9f2fd6df4ad7d3eb4dde4

--- main.dart.js
+++ main.dart.js
@@ -31464,7 +31464,7 @@ a.n(0,r.V(0,(q>0?Math.max(0,q-s):Math.min(0,q+s))-q))},
 yP(a){var s,r,q,p,o,n,m,l,k,j=this,i={},h=i.a=-a*2,g=j.Q
 if(g&&j.as)i.a=h*0.45
 else if(g||j.as)i.a=h*0.9
-if(!g||!j.as){s=j.geX()
+s=j.geX()
 if(j.Q&&j.db!=null){g=j.db.b.a
 r=Math.atan2(g[1],g[0])+1.5707963267948966}else if(j.as&&j.dx!=null){g=j.dx.b.a
 r=Math.atan2(g[1],g[0])+1.5707963267948966}else r=0
@@ -31475,7 +31475,8 @@ p=$.vk
 g=j.Q
 if(g&&!j.as)p=Math.abs(q)<$.vm?p*0.2:p*(1-$.vl)
 else if(j.as&&!g)p*=1+$.vj
-i.a=i.a+q*p}g=j.y
+i.a=i.a+q*p
+g=j.y
 g===$&&A.c()
 o=new A.E(new Float64Array(2))
 o.ad(g)
@@ -31645,7 +31646,7 @@ p===$&&A.c()
 p=q.O(0,p)
 q=n.w
 q===$&&A.c()
-n=A.e0(A.eH(B.vo,"RaceRider Prototype\nphysics v.34 - Pure Master/Slave\nState: "+r.b+"\nSpeed: "+B.d.J(Math.sqrt(p.O(0,q).aG(0,3).gcg()),1)+"\nAngle: "+B.d.J(n.geX(),2)+" rad"),B.n)
+n=A.e0(A.eH(B.vo,"RaceRider Prototype\nphysics v.35 - Pure Master/Slave\nState: "+r.b+"\nSpeed: "+B.d.J(Math.sqrt(p.O(0,q).aG(0,3).gcg()),1)+"\nAngle: "+B.d.J(n.geX(),2)+" rad"),B.n)
 n.cB()
 n.aN(a,B.tH)}}
 A.qJ.prototype={
