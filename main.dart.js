581a6ddbf553466896712b47d6d70ab84b999e07
34steve34 <34steve34@users.noreply.github.com>
2026-05-31 15:22:04 +0000

    deploy: d379c79642a6fab8a87f2a906baa65bc99bac8a4

--- main.dart.js
+++ main.dart.js
@@ -32452,7 +32452,8 @@ a.aB(-r,b)
 a.i2()
 a0=Math.atan2(s[1],s[0])
 a1=-b5.b*212750
-if(a1>0){if(b5.w)a1*=0.12}else if(a1<0)a1*=0.38
+if(a1<0)a1*=0.38
+else if(a1>0)a1=b5.w?a1*0.12:a1
 b5.e=a1
 a2=b5.r&&!b5.w?q*Math.sin(a0-Math.atan2(3.8,9.2))*9.2/19.5:0
 s=b5.r
